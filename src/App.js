19fe8c64aa496b9d6e94713973fd58df5f9494cf
Piyush@laptop <chandra7piyush@gmail.com>
2023-12-26 19:45:35 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+//installed eslint. just run this in terminal "npm run lint"
+import React from "react";
+import Header from "./components/Header";
+
+function App() {
+    return (
+        <div>
+            <Header />
+            App
+        </div>
+    );
+}
+
+export default App;

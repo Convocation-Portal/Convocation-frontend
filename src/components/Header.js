c63cd2901216e676d6c1a5f9a06dafa990b438ca
GayatriXJadhav <gayatrijadhav1202@gmail.com>
2024-01-14 12:59:24 +0530

    header changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 const Header = () => {
     return (
         <>
-            <div>Header</div>
+            <div>Header here</div>
             <Outlet />
             {/*Outlet ke bina header ke niche wala nhi aayega mat htana*/}
         </>

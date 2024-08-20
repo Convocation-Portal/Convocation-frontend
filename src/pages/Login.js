6f826c2020db49edca0608c0b8fdd9e9d90eca1e
Jaskaran Singh <jaskaransingh858585@gmail.com>
2024-08-21 01:45:33 +0530

    upadted the summary and the loginpage

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -6,62 +6,78 @@ import { Link, useNavigate } from "react-router-dom";
 import Input from "../components/Input";
 
 const Login = () => {
-    const redirect = useNavigate();
-    const [formData, setFormData] = useState({
-        email: "",
-        password: "",
+  const redirect = useNavigate();
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    login(formData, redirect);
+  };
+
+  const handleFormReset = () => {
+    setFormData({
+      email: "",
+      password: "",
     });
+  };
 
-    const handleFormSubmit = (e) => {
-        e.preventDefault();
-        login(formData, redirect);
-    };
+  const handleSignup = () => {
+    redirect("/register");
+  };
 
-    const handleFormReset = () => {
-        setFormData({
-            email: "",
-            password: "",
-        });
-    };
+  return (
+    <div className={style.container}>
+      <div>
+        <h1>Login</h1>
+        <form onSubmit={handleFormSubmit} className={style.form}>
+          <Input
+            label="Email"
+            type="email"
+            name="email"
+            placeholder="Type your email address"
+            value={formData.email}
+            set={setFormData}
+          />
+          <Input
+            label="Password"
+            type="password"
+            name="password"
+            placeholder="Type your password"
+            value={formData.password}
+            set={setFormData}
+          />
+          <Link className="linkreset" to="/forgot-password">
+            Forgot Password?
+          </Link>
+          <div className={style.formbuttons}>
+            <button className={style.submitbutton} type="submit">
+              <MdOutlineKey /> Login
+            </button>
+            <button
+              className={style.resetbutton}
+              type="button"
+              onClick={handleFormReset}
+            >
+              Reset Form
+            </button>
+          </div>
 
-    return (
-        <div className={style.container}>
-            <div>
-                <h1>Login</h1>
-                <form onSubmit={handleFormSubmit} className={style.form}>
-                    <Input
-                        label="Email"
-                        type="email"
-                        name="email"
-                        placeholder="Type your email address"
-                        value={formData.email}
-                        set={setFormData}
-                    />
-                    <Input
-                        label="Password"
-                        type="password"
-                        name="password"
-                        placeholder="Type your password"
-                        value={formData.password}
-                        set={setFormData}
-                    />
-                    <Link to="/forgot-password">Forgot Password?</Link>
-                    <div className={style.formbuttons}>
-                        <button className={style.submitbutton} type="submit">
-                            <MdOutlineKey /> Login
-                        </button>
-                        <button
-                            className={style.resetbutton}
-                            type="button"
-                            onClick={handleFormReset}
-                        >
-                            Reset
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    );
+          <div className={style.signupContainer}>
+            <button
+              className={style.signupbutton}
+              type="button"
+              onClick={handleSignup}
+            >
+              Sign Up
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
